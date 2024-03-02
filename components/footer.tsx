875c3eec8586602af4e7bf920fa51f68619d70f0
dhanu1402 <dhanukumar2000@gmail.com>
2024-03-03 00:45:24 +0530

    navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer bg-white border-t>
+    <footer className="bg-white border-t">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
           &copy; 2024 Artsee, Inc. All rights reserved.
