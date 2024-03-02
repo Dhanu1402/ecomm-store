3457b2171ef8d6cc9eb778312883c35bfd05de90
dhanu1402 <dhanukumar2000@gmail.com>
2024-03-02 23:13:26 +0530

    project setup with footer

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer bg-white border-t>
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2024 Artsee, Inc. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
