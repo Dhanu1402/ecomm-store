596b874f8e613d14e1f4b83f4b820f92546f89b5
dhanu1402 <dhanukumar2000@gmail.com>
2024-03-03 20:41:27 +0530

    individual product screen

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,6 +1,5 @@
-import qs from 'query-string';
-
 import { Product } from '@/types';
+import qs from 'query-string';
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
@@ -12,9 +11,8 @@ interface Query {
 }
 
 const getProducts = async (query: Query): Promise<Product[]> => {
-  const url = qs.stringify({
+  const url = qs.stringifyUrl({
     url: URL,
-
     query: {
       colorId: query.colorId,
       sizeId: query.sizeId,
@@ -23,7 +21,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
     },
   });
 
-  const res = await fetch(URL);
+  const res = await fetch(url);
 
   return res.json();
 };
