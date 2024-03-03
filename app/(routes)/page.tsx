cd197f324956fa05853889e0fd76a670d60142c2
dhanu1402 <dhanukumar2000@gmail.com>
2024-03-03 13:21:33 +0530

    billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>Hello</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard('86544eb2-8523-47bb-a5fd-6f75c94445d1');
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
